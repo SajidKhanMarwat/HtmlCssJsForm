0285a0f9a8038af4453e134f7d141ecd63dfa3da
SajidKhanMarwat <70630859+SajidKhanMarwat@users.noreply.github.com>
2022-12-23 15:03:25 +0500

    Delete script.js

--- HEC Form/Script/script.js
+++
@@ -1,6 +0,0 @@
-function display(){
-    var imgcanvas = document.getElementById("imgd");
-    var fileinput = document.getElementById("myFile");
-    var image = new SimpleImage(fileinput);
-    image.drawTo(imgcanvas);
-}
